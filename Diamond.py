94ecb991f4637e289447c6fcc1c114138c310d59
Thomas McClellan <thomasmcclellan87@gmail.com>
2018-01-26 16:09:57 -0500

    Initial commit

---
+++ Diamond.py
@@ -0,0 +1,22 @@
+print('Please type a letter: ')
+l = str(input())
+n = ord(l) - 96
+
+val = 64
+val += 1
+ch = chr(val)
+print((n - 1) * ' ' + ch)
+
+for i in range(1, n):
+  val += 1
+  ch = chr(val)
+  print((n - i - 1) * ' ' + ch + (2 * i - 1) * ' ' + ch)
+
+for i in reversed(range(1, n - 1)):
+  val -= 1
+  ch = chr(val)
+  print((n - i - 1) * ' ' + ch + (2 * i - 1) * ' ' + ch)
+
+val = 65
+ch = chr(val)
+print((n - 1) * ' ' + ch)
